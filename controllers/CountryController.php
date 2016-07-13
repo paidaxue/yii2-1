599a812c20089a36423438bd60b6b98b455fa255
cyangbo <yangbocai@YangboCai.snsshop.net>
2016-07-13 15:09:55 +0800

    导入yii2

---
+++ controllers/CountryController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Author: caiyangbo
+ * Date: 2016/7/11
+ * Time: 18:26
+ */
+
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+
+    /**
+     * @return string
+     * 显示国家
+     * http://apidebug.cxm/index.php?r=country/index
+     */
+    public function actionIndex()
+    {
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+}
+
---
+++ views/site/say.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: caiyangbo
+ * Date: 2016/7/11
+ * Time: 17:34
+ */
+
+use yii\helpers\Html;
+
+?>
+
+<?=Html::encode($message) ?>
